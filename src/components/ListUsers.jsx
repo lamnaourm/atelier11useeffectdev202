1766b881938ac4d9e4d66ee9726b0d8beaba8c2c
lamnaourm <mohammed.lamnaour@gmail.com>
2023-10-31 12:27:57 +0100

    posts

--- src/components/ListUsers.jsx
+++ src/components/ListUsers.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import User from './User'
 
-function ListUsers({users}) {
+function ListUsers({ users }) {
+
+  const [activeId, setActiveId] = useState(0);
+
+
   return (
     <div>
       <h1>Nombre d'utilisateurs : {users.length}</h1>
       {
-        users.map(u => 
-          <User key={u.id} user={u} />
-          )
+        users.map(u =>
+          <User key={u.id} user={u} activeId={activeId} handleChange={(id) => setActiveId(id)} />
+        )
       }
     </div>
   )
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
-export default function User({user}) {
-  return (
-    <div>
-        <h1>Nom : {user.name}</h1>
-        <p>Email: {user.email}</p>
-        <p>Ville : {user.address.city} Rue : {user.address.street}</p>
-        <button>Details user</button>
-    </div>
-  )
+export default function User({ user, handleChange, activeId }) {
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        if(activeId===user.id){
+            const getPosts = async ()=>{
+                let res = await axios.get("https://jsonplaceholder.typicode.com/posts?userid="+activeId);
+                return res.data
+              }
+              getPosts().then((posts)=>setPosts(posts));
+        }
+    },[activeId])
+
+    return (
+        <div>
+            <h1>Nom : {user.name}</h1>
+            <p>Email: {user.email}</p>
+            <p>Ville : {user.address.city} Rue : {user.address.street}</p>
+            <button onClick={() => handleChange(user.id)} disabled={activeId===user.id}>Details user</button>
+        </div>
+    )
 }
